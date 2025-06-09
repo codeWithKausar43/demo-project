e602a463e15c9a0ba328d4ac760558089b1e83b2
Kausar Ahammed <kausar.a.pro@gmail.com>
2025-06-09 19:44:42 +0600

    trusted section complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import Navbar from "./components/Navbar"
 import banner from "./assets/banner.png"
+import Tusted from "./components/Tusted"
 
 function App() {
 
   return (
     <div>
       <header style={{
-        backgroundImage: `url(${banner})`, 
+      backgroundImage: `url(${banner})`, 
       backgroundSize: "cover",
       height: "90vh",
       width:"100%",
       backgroundposition:"center",
-      
+
       }} className="">
         <Navbar></Navbar>
       </header>
+      <Tusted></Tusted>
       
     </div>
   )
---
+++ src/components/Tusted.jsx
@@ -0,0 +1,16 @@
+function Tusted() {
+    return (
+        <div className="bg-[#DBFB1E] rounded-t-2xl -mt-6">
+            <div className="flex items-center max-w-7xl mx-auto py-6 justify-between">
+                <p>Trusted by leading company</p>
+                <p>FocalPont</p>
+                <p>Comment+R</p>
+                <p>GlobalBank</p>
+                <p>Alt+shipt</p>
+                <p>Lominius</p>
+                
+            </div>
+        </div>
+    )
+}
+export default Tusted
